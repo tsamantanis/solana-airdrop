b4b2f5756d62f72aea77d29fb2c86d4913a2194c
tsamantanis <philip.tsamantanis@icloud.com>
2022-01-06 19:08:37 +0200

    Create airDropSol function and a driver test function

--- index.js
+++ index.js
@@ -22,10 +22,31 @@ const {
       new PublicKey(myWallet.publicKey)
     );
     console.log(`=> For wallet address ${publicKey}`);
-    console.log(`Wallet balance: ${walletBalance}`);
+    console.log(`Wallet balance: ${parseInt(walletBalance)/LAMPORTS_PER_SOL} SOL`);
   } catch (err) {
       console.log(err);
   }
 };
 
-getWalletBalance()
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const myWallet = await Keypair.fromSecretKey(secretKey);
+    console.log(`-- Airdropping 2 SOL --`);
+    const fromAirDropSignature = await connection.requestAirdrop(
+      new PublicKey(myWallet.publicKey),
+      2 * LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (err) {
+      console.log(err);
+  }
+};
+
+const driverFunction = async () => {
+  await getWalletBalance();
+  await airDropSol();
+  await getWalletBalance();
+}
+
+driverFunction();
