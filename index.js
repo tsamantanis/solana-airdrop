98af110230ed598ea634c89ff5dd5c839ff782e0
tsamantanis <philip.tsamantanis@icloud.com>
2022-01-06 19:03:54 +0200

    Create getWalletBalance function

---
+++ index.js
@@ -0,0 +1,31 @@
+const {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  Transaction,
+  Account,
+ } = require("@solana/web3.js");
+
+ const newPair = new Keypair();
+
+ const publicKey = new PublicKey(newPair._keypair.publicKey).toString();
+
+ const secretKey = newPair._keypair.secretKey
+
+ const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const myWallet = await Keypair.fromSecretKey(secretKey);
+    const walletBalance = await connection.getBalance(
+      new PublicKey(myWallet.publicKey)
+    );
+    console.log(`=> For wallet address ${publicKey}`);
+    console.log(`Wallet balance: ${walletBalance}`);
+  } catch (err) {
+      console.log(err);
+  }
+};
+
+getWalletBalance()
